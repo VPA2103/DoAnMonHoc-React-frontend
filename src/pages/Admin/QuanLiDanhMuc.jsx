d09f3a2e949b5ab4a9ad14a740a98aaff99e8e02
Loi <nguyenduyloi05012005@mail.com>
2026-01-17 20:14:23 +0700

    nhap

--- src/pages/Admin/QuanLiDanhMuc.jsx
+++ src/pages/Admin/QuanLiDanhMuc.jsx
@@ -18,7 +18,7 @@ function QuanLiDanhMuc() {
     try {
       setLoading(true);
       const res = await getDanhMucs();
-      xbr-kbiq-xvf
+      setDanhMucs(res.data);
     } catch (err) {
       console.error("Lỗi load danh mục:", err);
       alert("Không tải được danh mục");
